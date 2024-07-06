957b29af17090851dcbde90ef5c05bae5477e5af
johannes <johannesblhansen@gmail.com>
2024-07-06 10:38:48 +0200

    Playing with async and tomcat embedded threads

--- javacode/rest/basic-rest-app/src/main/java/dk/johannes/basic_rest_app/BasicRestController.java
+++ javacode/rest/basic-rest-app/src/main/java/dk/johannes/basic_rest_app/BasicRestController.java
@@ -1,25 +1,48 @@
 package dk.johannes.basic_rest_app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 public class BasicRestController {
 
+    //Used for testing variables from docker
     @Value("${my.env.thing}")
     private String myEnvThingString;
+
+    private final AsyncRestGetter asyncRestGetter;
+
+    public BasicRestController(AsyncRestGetter asyncRestGetter) {
+        this.asyncRestGetter = asyncRestGetter;
+    }
 
     @GetMapping("/hello")
     public ResponseEntity<String> getHello(){
         System.out.println(myEnvThingString);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://my-producer-container:8085/getSomething", String.class);
+        //The my-producer-container is the url to be used inside a docker container, not used when starting directly on the machine
+//        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://my-producer-container:8085/getSomething", String.class); //Todo make property
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:8085/getSomething", String.class);
 
         String body = forEntity.getBody();
 
         return ResponseEntity.ok("Hello from in here and response from producer is: " + body);
     }
+
+    @GetMapping("/helloAsync")
+    public ResponseEntity<String> getHelloAsync(){
+        for (int i = 0; i < 100; i++) {
+            log.info("Starting thread: {}", i);
+            asyncRestGetter.getHelloFromProducer();
+        }
+        String body = "All threads started";
+        return ResponseEntity.ok("Hello from in here and response from producer is: " + body);
+    }
+
+
 }
--- notes/docker/homegrown examples/simple rest app/Dockerizing a simple app.md
+++ notes/docker/homegrown examples/simple rest app/Dockerizing a simple app.md
@@ -1,4 +1,6 @@
 Dockerizing a simple Spring Boot Rest App
 #own_examples
 
+
+
 [Home Grown Example 1 - Simple Rest API app](Home%20Grown%20Example%201%20-%20Simple%20Rest%20API%20app.md)
